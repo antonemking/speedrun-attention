748747517a6ba97173ca10fe8fb061d29f330122
Antone King <toneking972@gmail.com>
2024-09-17 12:28:34 -0400

    netlify form

--- src/components/EmailCapture.js
+++ src/components/EmailCapture.js
@@ -26,12 +26,12 @@ function EmailCapture() {
     <div>
       {!submitted ? (
         <form
-          name="email-capture"
+          name="contact"
           method="POST"
-          data-netlify="true"
           netlify-honeypot="bot-field" // Anti-spam honeypot
           onSubmit={handleSubmit}
           className="max-w-md mx-auto bg-white p-6 rounded-lg shadow-lg"
+          netlify
         >
           {/* Netlify hidden input fields */}
           <input type="hidden" name="form-name" value="email-capture" />
